9f5e3fe3ce13e8a1b2ed56dfe6ade98bf6a56f01
Anano Topuria <anano.topuriaa@gmail.com>
2024-05-23 16:07:49 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Tours from "./components/Tours";
+import Services from "./components/Services";
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
